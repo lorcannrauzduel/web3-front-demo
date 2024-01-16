a1eb91204e9f0c7185462619b39fcdeffd7fd7e2
Lorcann Rauzduel <contact@lorcannrauzduel.fr>
2024-01-16 13:56:20 +0100

    refactored

--- constantes/alchemy.ts
+++
@@ -1,9 +0,0 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-
-// Optional config object, but defaults to the API key 'demo' and Network 'eth-mainnet'.
-const settings = {
-	apiKey: 'h5DGz8h2hf2mI8WPeoE_zKFMUBBotnHN', // Replace with your Alchemy API key.
-	network: Network.ETH_SEPOLIA, // Replace with your network.
-};
-
-export const alchemy = new Alchemy(settings);
